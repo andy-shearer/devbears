cd13f665f3a48d36e986c741b90fc1c8136d2b47
Andy Shearer <andyshearer.95@googlemail.com>
2022-04-22 16:57:38 +0100

    Handle random (ish) background selection

--- site/pages/ImageBuilder.js
+++ site/pages/ImageBuilder.js
@@ -1,11 +1,23 @@
 import mergeImages from "merge-images";
 const { Canvas, Image } = require('canvas');
 
+function getBackgroundNum(input) {
+  let count = 0;
+  for(let i=0; i<5; i++) {
+    count += parseInt(input[i]);
+  }
+
+  return (count % 2) + 1;
+}
+
 export default async function buildImage(input) {
   console.log("Input string for new bear:", input)
   const imgPrefix = "data:image/png;base64,";
+
+  const bgNum = getBackgroundNum(input);
+
   const base64 = await mergeImages(
-    ["./bearbuild/background.png", "./bearbuild/bearbody.png", "./bearbuild/bearhead.png" ],
+    [`./bearbuild/background${bgNum}.png`, "./bearbuild/bearbody.png", "./bearbuild/bearhead.png" ],
     {
       Canvas: Canvas,
       Image: Image
